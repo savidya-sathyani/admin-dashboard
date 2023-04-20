bf3d5dc4d762c98ba21bf2f525eb7c3b1f5035a3
Savidya Sathyani <mails.savidya@gmail.com>
2023-04-20 19:56:21 +1000

    Add side bar and two menu items

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import { ProSidebarProvider } from "react-pro-sidebar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./themes";
 import TopBar from "./scenes/global/TopBar";
+import SideBar from "./scenes/global/SideBar";
+import Dashboard from "./scenes/dashboard";
 
 const App = () => {
   const [theme, colorMode] = useMode();
@@ -10,8 +14,14 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <ProSidebarProvider>
+            <SideBar />
+          </ProSidebarProvider>
           <main className="content">
             <TopBar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -1,8 +1,53 @@
+import { useState } from "react";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { MenuOutlined } from "@mui/icons-material";
+import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
+import { tokens } from "../../themes";
+import userImage from "../../assets/images/user.jpg";
+
 const SideBar = () => {
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+	// const { collapseSidebar } = useProSidebar();
+	const [isCollapsed, setIsCollapsed] = useState(false);
+	const [selected, setSelected] = useState('Dashboard');
+
 	return (
-		<div className="side-bar">
-			<p>Side Bar</p>
-		</div>
+
+		<Sidebar defaultCollapsed={isCollapsed} transitionDuration={600}>
+			<Box>
+				<Menu menuItemStyles={{
+					button: () =>
+					({
+						"&:hover": {
+							backgroundColor: "transparent !important",
+						}
+					})
+				}}>
+					<MenuItem>
+						<Box display="flex" justifyContent={!isCollapsed ? "flex-end" : "center"} alignItems="center">
+							<IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+								<MenuOutlined />
+							</IconButton>
+						</Box>
+					</MenuItem>
+
+					{/* USER */}
+					{!isCollapsed && <Box>
+						<Box display="flex" justifyContent="center" alignItems="center" mb="2rem" mt="1rem">
+							<img alt="Profile User" width="100px" height="100px" src={userImage} style={{
+								cursor: "pointer", borderRadius: "50px"
+							}}
+							/>
+						</Box>
+						<Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" mb="2rem">
+							<Typography>Savidya Sathyani</Typography>
+							<Typography>VP Admin</Typography>
+						</Box>
+					</Box>}
+				</Menu>
+			</Box>
+		</Sidebar>
 	);
 };
 
